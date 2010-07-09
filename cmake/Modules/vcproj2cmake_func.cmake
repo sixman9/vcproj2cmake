32692f3571081e81fe91cb4a1a0096548d78784f
Andreas Mohr <andim2@users.sourceforge.net>
2010-07-09 14:45:00 +0200

    Minor improvements

--- cmake/Modules/vcproj2cmake_func.cmake
+++ cmake/Modules/vcproj2cmake_func.cmake
@@ -3,6 +3,12 @@
 
 # Some helper functions to be used by all converted projects in the tree
 
+# avoid useless repeated parsing
+if(V2C_FUNC_DEFINED)
+  return()
+endif(V2C_FUNC_DEFINED)
+set(V2C_FUNC_DEFINED true)
+
 # Function to automagically rebuild our converted CMakeLists.txt
 # by the original converter script in case any relevant files changed.
 function(v2c_rebuild_on_update _target_name _vcproj _cmakelists _script _master_proj_dir)
@@ -18,11 +24,13 @@ function(v2c_rebuild_on_update _target_name _vcproj _cmakelists _script _master_
       VERBATIM
     )
 
-    #add_custom_target(${_target_name}_build_cmakelists VERBATIM SOURCES ${_cmakelists})
-    add_custom_target(${_target_name}_build_cmakelists ALL VERBATIM SOURCES ${_cmakelists})
+    #add_custom_target(${_target_name}_update_cmakelists VERBATIM SOURCES ${_cmakelists})
+    add_custom_target(${_target_name}_update_cmakelists ALL VERBATIM SOURCES ${_cmakelists})
 
+  if(TARGET ${_target_name}) # in some projects an actual target might not exist (i.e. we simply got passed the project name)
     # make sure the rebuild happens _before_ trying to build the actual target.
-    add_dependencies(${_target_name} ${_target_name}_build_cmakelists)
+    add_dependencies(${_target_name} ${_target_name}_update_cmakelists)
+  endif(TARGET ${_target_name})
 
 # FIXME!!: we should definitely achieve aborting build process directly
 # after a new CMakeLists.txt has been generated (we don't want to go
@@ -34,12 +42,13 @@ function(v2c_rebuild_on_update _target_name _vcproj _cmakelists _script _master_
 # Which is even harder to achieve, I guess... (set a marker variable
 # or marker file and check for it somewhere global at the end of it all,
 # then abort, that would be the idea)
-#  add_custom_target(build_cmakelists_abort_build ALL
+#  add_custom_target(update_cmakelists_abort_build ALL
 ##    COMMAND /bin/false
 #    COMMAND sdfgsdf
 #    DEPENDS "${_cmakelists}"
+#    VERBATIM
 #  )
 #
-#  add_dependencies(build_cmakelists_abort_build build_cmakelists)
+#  add_dependencies(update_cmakelists_abort_build update_cmakelists)
   endif(NOT V2C_PREVENT_AUTOMATIC_REBUILD)
 endfunction(v2c_rebuild_on_update _target_name _vcproj _cmakelists _script _master_proj_dir)
