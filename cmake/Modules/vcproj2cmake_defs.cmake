bdf886abe561aa8e188ba33b91aa89baeeef41d7
Andreas Mohr <andim2@users.sourceforge.net>
2010-08-06 20:43:12 +0200

    Forgot to add V2C_LOCAL_CONFIG_DIR

--- cmake/Modules/vcproj2cmake_defs.cmake
+++ cmake/Modules/vcproj2cmake_defs.cmake
@@ -3,6 +3,8 @@ set(V2C_LIBS )
 set(V2C_LIB_DIRS )
 set(V2C_SOURCES )
 
+set(V2C_LOCAL_CONFIG_DIR ./cmake/vcproj2cmake)
+
 # Pre-define hook include filenames
 # (may be redefined/overridden by local content!)
 set(V2C_HOOK_PROJECT ${V2C_LOCAL_CONFIG_DIR}/hook_project.txt)
